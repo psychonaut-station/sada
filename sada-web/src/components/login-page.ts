bea345d1be50ef19045afe80dd91de122f6b8ba7
Sefa <50076109+Seefaaa@users.noreply.github.com>
2026-05-19 02:36:25 +0300

    lint/format

--- sada-web/src/components/login-page.ts
+++ sada-web/src/components/login-page.ts
@@ -3,13 +3,13 @@ import { customElement } from "lit/decorators.js";
 
 @customElement("login-page")
 export class LoginPage extends LitElement {
-  static styles = css`
-    :host {
-      display: block;
-    }
-  `;
+    static styles = css`
+        :host {
+          display: block;
+        }
+    `;
 
-  protected render(): TemplateResult {
-    return html``;
-  }
+    protected render(): TemplateResult {
+        return html``;
+    }
 }
